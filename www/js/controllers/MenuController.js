9542fdbb0b6594f56349cbeed079f6ad8dbe29b4
Andrew-Wichmann <andrewwichmann@siu.edu>
2017-03-07 12:07:06 -0600

    The menu now contains the restaurant name which is also pulled from Firebase

--- www/js/controllers/MenuController.js
+++ www/js/controllers/MenuController.js
@@ -1,39 +1,17 @@
-app.controller('MenuController', ['$scope', 'menudatabase', function($scope, menudatabase) {
+app.controller('MenuController', ['$scope', 'menudatabase', 'restaurantnamedatabase', function($scope, menudatabase, restaurantnamedatabase) {
+  
+  
   menudatabase.success(function(data) {
     $scope.menu = data
-  });/*
+  });
+  
+  
   restaurantnamedatabase.success(function(data) {
-    console.log(data);
-    $scope.restaurant_name = data
-  })
+    $scope.restaurant_name = data;
+    console.log("Debug: MenuController.js set $scope.restaurant_name = " + $scope.restaurant_name);
+  });
   
   
-  /*$scope.menu = [
-    { 
-      image: 'img/move.jpg', 
-      dish: 'MOVE', 
-      discription: 'MOVE, Inc.', 
-      price: 0.99 
-	  }, 
-    { 
-      image: 'img/shutterbugg.jpg', 
-      dish: 'Shutterbugg', 
-      discription: 'Chico Dusty', 
-      price: 2.99 
-    },
-    {
-      image: 'img/forecast.jpg',
-      dish: 'HeadSpace',
-      discription: 'HeadSpace LLC',
-      price: 1.99
-    },
-    {
-      image: 'img/gameboard.jpg',
-      dish: 'Mint',
-      discription: 'Intuit Inc',
-      price: 9.99
-    },
-  ];*/
 }]);
 
 console.log("Menucontroller loaded");

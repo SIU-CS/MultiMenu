ad2b68e66e16558bc83dc7622c72229e4e3f6056
Andrew-Wichmann <andrewwichmann@siu.edu>
2017-03-28 21:17:49 -0500

    Added currency exchange using real data but it is static from USD to RUP rn.

--- www/js/controllers/MenuController.js
+++ www/js/controllers/MenuController.js
@@ -1,4 +1,4 @@
-app.controller('MenuController', ['$scope', 'menudatabase', 'restaurantnamedatabase', function($scope, menudatabase, restaurantnamedatabase) {
+app.controller('MenuController', ['$scope', 'menudatabase', 'restaurantnamedatabase', 'exchangerates', function($scope, menudatabase, restaurantnamedatabase, exchangerates) {
   
   
   menudatabase.success(function(data) {
@@ -11,6 +11,13 @@ app.controller('MenuController', ['$scope', 'menudatabase', 'restaurantnamedatab
     console.log("Debug: MenuController.js set $scope.restaurant_name = " + $scope.restaurant_name);
   });
   
+  $('#button').click(function(){
+    exchangerates.success(function(data)  {
+      for(var i=0; i<$scope.menu.length; i++){
+        $scope.menu[i].price = fx.convert($scope.menu[i].price, {from: "USD", to: "RUB"});
+      }
+    });
+  })
   
 }]);
 
---
+++ www/js/services/exchange_rates.js
@@ -0,0 +1,21 @@
+app.factory('exchangerates', ['$http', function($http) {
+  return $http.get('https://openexchangerates.org/api/latest.json?app_id=6625bde1b559456a8f2a57573195105a') 
+    .success(function(data) { 
+      console.log("Got exchange rates.");
+      if ( typeof fx !== "undefined" && fx.rates ) {
+        fx.rates = data.rates;
+        fx.base = data.base;
+      } else {
+      // If not, apply to fxSetup global:
+        var fxSetup = {
+          rates : data.rates,
+          base : data.base
+        }
+      };
+      return data; 
+    })
+    .error(function(err) {
+      console.log("Could not get exchange rates.")
+      return err;
+    });
+}]);

ee7d4d24be197a878e7f765841067b86e701fb09
Andrew-Wichmann <andrewwichmann@siu.edu>
2017-03-30 22:51:19 -0500

    The directive now takes the selection from the dropdown, but the menu does now yet update.

--- www/js/directives/converter-dropdown.js
+++ www/js/directives/converter-dropdown.js
@@ -1,7 +1,9 @@
 app.directive('converter', function() { 
   return { 
     restrict: 'EA', 
-    scope: {}, 
+    scope: {
+      menu: '=',
+    },
     templateUrl: 'views/converter-dropdown.html',
     link: function(scope, element, attributes){
       $('document').ready(function(){
@@ -9,8 +11,12 @@ app.directive('converter', function() {
           var symbol = Object.keys(fx.rates)[i];
           console.log(symbol);
           $("#currency-drop-down").append('<li class='+i+'>' + symbol + '</li>');          
-          $("."+i).on('click', function(symbol){
-            console.log(symbol);
+          $("."+i).on('click', function(){
+            for(var j=0; j<Object.keys(scope.menu).length; j++){
+              //console.log(fx.convert(scope.menu[j].price, {from: "USD", to: this.innerHTML}));
+              scope.menu[j].price = fx.convert(scope.menu[j].price, {from: "USD", to: this.innerHTML});
+              console.log(scope.menu[j].price);
+            }
           });
       }
       });
